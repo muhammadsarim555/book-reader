82011f113e6a4bf72f13e13be5cbdd771111d572
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-04-21 16:20:41 +0500

    Header Implement

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -2,14 +2,48 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
 // PACKAGES
-import { Container, Header, Left, Body, Right, Button, Icon, Title  } from 'native-base';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title
+} from "native-base";
 
 export default class Home extends Component {
+  static navigationOptions = {
+    header: null,
+    headerStyle: {
+      backgroundColor: "red"
+    }
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Home!</Text>
-      </View>
+      <Container>
+        <Header androidStatusBarColor="#FFDD0D">
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title style={{ alignSelf: "center" }}>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Text>Cancel</Text>
+            </Button>
+          </Right>
+        </Header>
+
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Home!</Text>
+        </View>
+      </Container>
     );
   }
 }
@@ -19,7 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#FFDD0D"
   },
   welcome: {
     fontSize: 20,
